0ac30d397f384ffd40f3f1a8a1b5800e49d97937
AlanAlv <alan.alv96@gmail.com>
2020-06-13 04:16:09 -0500

    Create user route

--- index.js
+++ index.js
@@ -10,6 +10,9 @@ connectDB();
 // App port
 const PORT = process.env.PORT || 4000;
 
+// Import Routes
+app.use('/api/users', require('./routes/users'))
+
 // Start app
 app.listen(PORT, () =>  {
     console.log(`Server running on port ${PORT}`);
