8a28b35835a9d3b7cd70a3f840d7612b8d3731c3
AlanAlv <alan.alv96@gmail.com>
2020-06-13 03:42:27 -0500

    Created express server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('Hello World');
+const express = require('express');
+
+// Create server
+const app = express();
+
+// App port
+const PORT = process.env.PORT || 4000;
+
+// Start app
+app.listen(PORT, () =>  {
+    console.log(`Server running on port ${PORT}`);
+});
