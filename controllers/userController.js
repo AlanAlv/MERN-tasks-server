b503673eab9912f3ac782953409b17cdeb97979b
AlanAlv <alan.alv96@gmail.com>
2020-06-14 02:38:21 -0500

    Create new user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,27 @@
+const User = require('../models/User')
 
 
+exports.createUser = async (req, res) => {
 
-exports.createUser = (req, res) => {
-    console.log(req.body)
+    // Destructure email & password
+    const {email, password } = req.body;
+    try {
+        let user = await User.findOne({ email });
+
+        if (user) {
+            return res.status(400).json({msg: 'The user already exists'})
+        }
+
+        // Create new user
+        user =  new User(req.body);
+
+        // Save user
+        await user.save();
+
+        // Confirmation message
+        res.json({ msg:'User created correctly'});
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('There was am error');
+    }
 }
